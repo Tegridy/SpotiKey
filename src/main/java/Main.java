b87e854482bda06e9fd1b76ddef1883612bc0de7
Tegridy <jarjak4@gmail.com>
2021-07-01 16:04:17 +0200

    Move AHK script to separate file
    Move ahk script and do code refractor

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import hotkey.AutoHotkeyLoader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AutoHotkeyLoader autoHotkeyLoader = new AutoHotkeyLoader();
+    }
+}
---
+++ src/main/java/hotkey/AutoHotkeyDll.java
@@ -0,0 +1,16 @@
+package hotkey;
+
+import com.sun.jna.Library;
+import com.sun.jna.WString;
+
+public interface AutoHotkeyDll extends Library {
+    void ahkExec(WString s);
+
+    void ahkDll(WString s, WString o, WString p);
+
+    void addFile(WString s, int a);
+
+    void ahkTextDll(WString s, WString o, WString p);
+
+    void ahkFunction(WString f, WString p1);
+}
