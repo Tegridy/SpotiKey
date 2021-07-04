1c34d991abceb3794674c5c8833a3fbc7e29a272
Tegridy <jarjak4@gmail.com>
2021-07-04 19:38:00 +0200

    Add JavaFX dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import keystroke.KeystrokeListener;
 import player.PlayerController;
 import user.Config;
 import user.LoadConfig;
 import user.SaveConfig;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) {
-      //  KeystrokeListener keystrokeListener = new KeystrokeListener();
-        Config config = new Config();
-      //  SaveConfig saveConfig = new SaveConfig(config);
-        LoadConfig loadConfig = new LoadConfig(config);
 
-        System.out.println(config.toString());
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
+
+        stage.setTitle("SpotiKey");
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
